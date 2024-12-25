629c5dbf72d65374531e0e0eec2d8de631f7a2ee
sprunkiincredibox <incredibox@sprunkix.com>
2024-12-25 13:44:22 +0800

    move videos and game up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ export default function Home() {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <Header />
-      <Introduction />
 	  <VideoSection />
 	  <GameSection />
+	  <Introduction />
       <CharacterFeatures />
       <CharacterForms />
       <GameModules />
