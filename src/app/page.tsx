67bb8905dd9304450962fc86c94b026081b1057c
sprunkiincredibox <incredibox@sprunkix.com>
2024-12-25 13:36:19 +0800

    change postion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <Header />
       <Introduction />
+	  <VideoSection />
+	  <GameSection />
       <CharacterFeatures />
       <CharacterForms />
-      <VideoSection />
-      <GameSection />
       <GameModules />
       <Footer />
     </div>
