2a19093a6a8afc7425faea677db218d44db7b80a
SprunkiIncredibox <incredibox@sprunkix.com>
2024-12-26 07:16:17 +0800

    fix error

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -8,7 +8,7 @@ export default function GoogleAnalytics({ measurementId }: { measurementId: stri
         src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
       />
 	  <script defer data-domain="sprunkilily.com" src="https://app.pageview.app/js/script.js"/>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0831789447587581" crossorigin="anonymous"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0831789447587581" crossOrigin="anonymous"/>
       <script
         dangerouslySetInnerHTML={{
           __html: `
