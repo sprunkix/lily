997a3054143782e5b119aec637a54e3fb1b31a89
sprunkiincredibox <incredibox@sprunkix.com>
2024-12-27 08:27:46 +0800

    index relatedGame

--- src/components/game/RelatedGames.tsx
+++ src/components/game/RelatedGames.tsx
@@ -12,36 +12,37 @@ export default async function RelatedGames({ currentGame, lang }: RelatedGamesPr
 	const relatedGames = [
 	  {
 		title: "Sprunki Ketchup 2.0",
-		imageUrl: "https://sprunkiketchup.pro/images/sprunki-ketchup-2.jpg",
+		ogImage: "https://sprunkiketchup.pro/images/sprunki-ketchup-2.jpg",
 		gameUrl: "https://sprunkiketchup.pro/"
 	  },
 	  {
 		title: "Sprunki Retake",
-		imageUrl: "https://sprunkix.com/images/sprunki-retake.png",
+		ogImage: "https://sprunkix.com/images/sprunki-retake.png",
 		gameUrl: "https://sprunkix.com/game/sprunki-retake/"
 	  },
 	  {
 		title: "Sprunki Phase 10",
-		imageUrl: "https://sprunkix.com/images/sprunki-phase-10.jpg",
+		ogImage: "https://sprunkix.com/images/sprunki-phase-10.jpg",
 		gameUrl: "https://sprunkix.com/game/sprunki-phase-10/"
 	  }
 	  ,
 	  {
 		title: "Sprunki Mustard",
-		imageUrl: "https://sprunkix.com/images/mustard.webp",
+		ogImage: "https://sprunkix.com/images/mustard.webp",
 		gameUrl: "https://sprunkix.com/game/sprunki-mustard/"
 	  }
 	];
   return (
     <div className="mt-8">
       <h3 className="text-2xl font-bold text-text-primary mb-4">Related Games</h3>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
         {relatedGames.map((game) => (
           <RelatedGame
-            key={game.slug}
+            key={game.title}
             title={game.title}
             imageUrl={game.ogImage}
-            gameUrl={`/${lang}/games/${game.slug}`}
+			gameUrl={game.gameUrl}
+            //gameUrl={`/${lang}/games/${game.slug}`}
           />
         ))}
       </div>
