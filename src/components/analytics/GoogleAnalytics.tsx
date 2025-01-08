595178a295f4776da0001050cc6e01ffb8d20b10
sprunkiincredibox <incredibox@sprunkix.com>
2025-01-08 10:48:01 +0800

    Update GoogleAnalytics.tsx
    dell consent denied

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -9,16 +9,16 @@ export default function GoogleAnalytics({ measurementId }: { measurementId: stri
       dangerouslySetInnerHTML={{
         __html: `
         // Define dataLayer and the gtag function.
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        //window.dataLayer = window.dataLayer || [];
+        //function gtag(){dataLayer.push(arguments);}
         // Set default consent to 'denied' as a placeholder
         // Determine actual values based on your own requirements
-        gtag('consent', 'default', {
-        'ad_storage': 'denied',
-        'ad_user_data': 'denied',
-        'ad_personalization': 'denied',
-        'analytics_storage': 'denied'
-        });
+        //gtag('consent', 'default', {
+        //'ad_storage': 'denied',
+        //'ad_user_data': 'denied',
+        //'ad_personalization': 'denied',
+        //'analytics_storage': 'denied'
+        //});
         `
     }}
     />
