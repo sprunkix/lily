10a236a62e706d6d8ffde79be96898650e9e58b8
sprunkiincredibox <incredibox@sprunkix.com>
2025-01-08 10:26:21 +0800

    script 加载方式

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -1,14 +1,13 @@
 'use client'
 
-import Script from "next/script"
-
 export default function GoogleAnalytics({ measurementId }: { measurementId: string }) {
   console.log(process.env.NODE_ENV);
   if (process.env.NODE_ENV !== 'development') {
   return (
     <>
-    <Script strategy="afterInteractive">
-      {`
+    <script 
+      dangerouslySetInnerHTML={{
+        __html: `
         // Define dataLayer and the gtag function.
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
@@ -20,33 +19,34 @@ export default function GoogleAnalytics({ measurementId }: { measurementId: stri
         'ad_personalization': 'denied',
         'analytics_storage': 'denied'
         });
-        `}
-    </Script>
-    <Script strategy="lazyOnload" async src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`} />
-	  <Script strategy="afterInteractive" defer data-domain="sprunkilily.com" src="https://app.pageview.app/js/script.js" />
-	  <Script strategy="lazyOnload" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0831789447587581" crossOrigin="anonymous" />
-    <Script strategy="afterInteractive">
-      {`
+        `
+    }}
+    />
+    <script async src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`} />
+	  <script defer data-domain="sprunkilily.com" src="https://app.pageview.app/js/script.js" />
+	  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0831789447587581" crossOrigin="anonymous" />
+    <script dangerouslySetInnerHTML={{
+      __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${measurementId}');
-        `}
-    </Script>
-    <Script strategy="afterInteractive">
-        {`
+        `}}
+    />
+    <script dangerouslySetInnerHTML={{
+      __html: `
           (function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('groleegni.net',8751550,document.createElement('script'))
-           `}
-    </Script>
-    <Script strategy="afterInteractive">
-        {`
-          function consentGrantedAdStorage() {
-              gtag('consent', 'update', {
-                'ad_storage': 'granted'
-              });
-            }
-          `}
-      </Script>
+           `}}
+    />
+    <script dangerouslySetInnerHTML={{
+        __html: `
+            function consentGrantedAdStorage() {
+                gtag('consent', 'update', {
+                  'ad_storage': 'granted'
+                });
+              }
+            `}}
+      />
     </>
   )
   } else {
