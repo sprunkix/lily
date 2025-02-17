20856eae4de6f36cd2623636de8be04e86d8d5a1
SprunkiIncredibox <incredibox@sprunkix.com>
2025-02-17 22:14:04 +0800

    google

--- src/components/ads/ads.tsx
+++ src/components/ads/ads.tsx
@@ -7,7 +7,12 @@ export default function Ads() {
     <>
     <GoogleAnalytics gaId={GA_MEASUREMENT_ID} />
 	  <script defer data-domain="sprunkilily.com" src="https://app.pageview.app/js/script.js" />
-	  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0831789447587581" crossOrigin="anonymous" />      
+	  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0831789447587581" crossOrigin="anonymous" />   
+    <script async src="https://fundingchoicesmessages.google.com/i/pub-0831789447587581?ers=1"></script>
+    <script  dangerouslySetInnerHTML={{
+        __html: `(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();`
+      }}
+      />
     </>
   )
   } else {
